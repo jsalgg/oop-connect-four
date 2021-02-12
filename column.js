b3ba599a77d6e89f8ee0522ebce22d6c0ed629ba
Luke  Kingsley <mzootfb@Lukes-MBP.attlocal.net>
2021-02-11 19:06:11 -0500

    placing token

---
+++ column.js
@@ -0,0 +1,16 @@
+export class Column {
+  constructor(tokenTracker = ["", "", "", "", "", ""]) {
+    this.tokenTracker = tokenTracker;
+  }
+  add(playerNumber) {
+    for (let i = this.tokenTracker.length - 1; i >= 0; i++) {
+      if (this.tokenTracker[i] === "") {
+        this.tokenTracker[i] = playerNumber;
+        return;
+      }
+    }
+  }
+  getTokenAt(i) {
+    return this.tokenTracker[i];
+  }
+}
